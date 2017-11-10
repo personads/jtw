32f5f89ece0efe5f172bf23188abb26ab4ed1924
Maximilian Müller-Eberstein <work@personads.me>
2017-11-10 23:16:06 +0100

    updated loss function

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from torch.autograd import Variable
+
 COMMAND_PROPERTIES = ['accelerator', 'brake', 'gear', 'steering', 'focus']
 STATE_PROPERTIES = [
     'angle',
@@ -26,3 +28,16 @@ def state_to_dict(state):
     for prop in STATE_PROPERTIES:
         res[prop] = eval('state.'+prop)
     return res
+
+def dict_to_vector(dict_in, properties, requires_grad):
+    res = []
+    for prop in properties:
+        if type(dict_in[prop]) is tuple:
+            for val in dict_in[prop]:
+                res.append(val)
+        else:
+            res.append(dict_in[prop])
+    return Variable(res, required_grad=requires_grad)
+
+def state_to_vector(state):
+    return dict_to_vector(state_to_dict(state), STATE_PROPERTIES, False)
--- utils/loss.py
+++ utils/loss.py
@@ -6,19 +6,27 @@ from collections import defaultdict
 
 from config import *
 
-#class RacingLoss(torch.nn.Criterion):
 class RacingLoss:
     def __init__(self, weights={'current_lap_time': .0, 
                                 'damage': .05, 
                                 'distance_raced': 0.5, 
                                 'race_position': .1,
                                 'speed_x': .1,
-                                'speed_y': .05,
                                 'distances_from_edge': .1,
                                 'distance_from_center': .05}):
         self.weights = defaultdict(float, weights)
         self.output = None
         self.gradInput = None
+
+    def _get_property_value(self, prop, value):
+        res = value
+        if prop in ['distance_raced', 'race_position', 'speed_x', 'distances_from_edge']:
+            if value > 0:
+                res = 1/res
+            elif value == 0:
+                res = 1
+        res = np.abs(res)
+        return res
 
     def calc_loss(self, state):
         res = 0.
@@ -27,15 +35,9 @@ class RacingLoss:
             print(prop, state[prop])
             if type(state[prop]) is tuple:
                 for prop_val in state[prop]:
-                    prop_val = np.abs(prop_val)
-                    if prop == 'distaces_from_edge':
-                        prop_val = 1/prop_val if prop_val > 0 else 1
-                    res += self.weights[prop]/len(state[prop]) * prop_val
+                    res += self.weights[prop]/len(state[prop]) * self._get_property_value(prop, prop_val) 
             else:
-                prop_val = np.abs(state[prop])
-                if prop in ['distance_raced', 'race_position', 'speed_x']:
-                    prop_val = 1/prop_val if prop_val > 0 else 1
-                res += self.weights[prop] * prop_val
+                res += self.weights[prop] * self._get_property_value(prop, state[prop])
         return res
 
     def forward(self, state, _):
@@ -43,4 +45,5 @@ class RacingLoss:
         return self.output
 
     def backward(self, state, _):
-        return self.output
+        self.gradInput = self.output
+        return self.gradInput
