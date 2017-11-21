660cb56f8e4b51559a03cd8fe3f1834e60f22308
Maximilian Müller-Eberstein <work@personads.me>
2017-11-21 12:34:21 +0100

    slightly stabilized judas

--- drivers/judas.py
+++ drivers/judas.py
@@ -3,12 +3,15 @@ import numpy as np
 from pytocl.driver import Driver
 from pytocl.car import State, Command
 
+from utils.data import *
+
 class Judas(Driver):
     def __init__(self):
         super().__init__()
         self.epochCounter = 0
         self.steering = 0
         self.data = []
+        self.corrections = 0
 
     def save_data(self):
         filename = "data/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".csv"
@@ -47,15 +50,32 @@ class Judas(Driver):
         res.append(command.focus)
         self.data.append(res)
 
+    def smooth_steer(self, state, target, command):
+        steering_error = target - state.distance_from_center
+        if np.abs(steering_error) > .5:
+            steering_error = .5 * np.sign(steering_error)
+            steering_error = 0. if self.corrections < 1 else steering_error*(1/self.corrections)
+            self.corrections += .1
+            print("corrected:", steering_error, "\nstate:", state_to_dict(state))
+        else:
+            self.corrections = 0
+        command.steering = self.steering_ctrl.control(
+            steering_error,
+            state.current_lap_time
+        )
 
     def drive(self, carstate: State) -> Command:
         command = Command()
-        self.steer(carstate, 0.0, command)
+        self.smooth_steer(carstate, 0.0, command)
         # ACC_LATERAL_MAX = 6400 * 5
         # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
-        v_x = 50
+        v_x = 80
         self.accelerate(carstate, v_x, command)
+        if self.epochCounter % 500 == 0:
+            print(state_to_dict(carstate))
         self.epochCounter += 1
         self.append_data(command,carstate)
-        if self.epochCounter % 6000 == 0: self.save_data()
         return command
+    
+    def on_shutdown(self):
+        self.save_data()
