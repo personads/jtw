e4ba2b5aade196600552b4e70238f500151778f7
cxxxw <33231216+cxxxw@users.noreply.github.com>
2017-11-30 18:42:14 +0100

    Add files via upload

--- drivers/recmode.py
+++ drivers/recmode.py
@@ -43,30 +43,111 @@ class rec_mode_driver(Driver):
         if not command.gear:
             command.gear = carstate.gear or 1
 
+
+
     def drive(self, carstate: State) -> Command:
+
         command = Command()
-        if -1 < carstate.distance_from_center < 1:
+        #print(carstate.angle)
+        #print(carstate.distance_from_center)
+
+        if -1 < carstate.distance_from_center < 1 and -80 < carstate.angle < 80:
             current_state = state_to_vector(carstate)
             command_vector = self.jesus.take_wheel(current_state)
             command = vector_to_command(command_vector)
             self.calc_gear(command, carstate)
-            if self.epoch % 100 == 0:
-                print(command_vector)
+            if carstate.speed_x < 0 and carstate.rpm < 1000:
+                self.steering = 1
+                command.gear = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+
+            elif carstate.speed_x > 0 and carstate.rpm < 1000:
+                self.steering = -1
+                command.gear = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+
             self.epoch += 1
+            print(command)
+            return command
 
-        elif carstate.distance_from_center < -1 and carstate.angle > 0:
-            self.steering = 1
-            v_x = 50
-            self.accelerate(carstate, v_x, command)
-            self.calc_gear(command, carstate)
-            print('recmode')
+        # Reposition car on track
+        if -1 < carstate.distance_from_center < 1 and -180 < carstate.angle < -80:
+                self.steeringering = -1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = 1
+                print('recmode1')
 
-        elif (carstate.distance_from_center > 1) and carstate.angle < 0:
-            self.steering = 1
-            v_x = 50
-            self.accelerate(carstate, v_x, command)
-            self.calc_gear(command, carstate)
-            print('recmode')
+        elif -1 < carstate.distance_from_center < 1 and 80 < carstate.angle < 180:
+                self.steeringering = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = -1
+                print('recmode2')
+
+        # Car on right side of track
+        elif carstate.distance_from_center < -1:
+            if 0 < carstate.angle <= 90:
+                self.steeringering = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = -1
+                print('recmode3')
+
+            elif 90 < carstate.angle < 180:
+                self.steering = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = -1
+                print('recmode4')
+
+
+            elif -90 < carstate.angle <= 0:
+                self.steering = 0.5
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = 1
+                print('recmode5')
+
+            elif -180 < carstate.angle <= -90:
+                self.steering = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = 1
+                print('recmode6')
+
+
+        # Car on left side of track
+        elif carstate.distance_from_center > 1:
+            if -180 < carstate.angle <= -90:
+                self.steering = -1
+                v_x = 15
+                self.accelerate(carstate, v_x, command)
+                command.gear = -1
+                print('recmode7')
+
+            elif -90 < carstate.angle < 0:
+                self.steering = 1
+                v_x = 30
+                self.accelerate(carstate, v_x, command)
+                command.gear = 2
+                print('recmode8')
+
+            elif 0 < carstate.angle < 90:
+                self.steering = 1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = 1
+                print('recmode9')
+
+            elif 90 < carstate.angle < 180:
+                self.steering = -1
+                v_x = 20
+                self.accelerate(carstate, v_x, command)
+                command.gear = 1
+                print('recmode10')
 
         return command
 
