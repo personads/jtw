90c0f758bbb6582828006fe8e2a65d579eefecd8
cxxxw <33231216+cxxxw@users.noreply.github.com>
2017-12-02 22:23:49 +0100

    Add files via upload

--- drivers/recmode.py
+++ drivers/recmode.py
@@ -1,5 +1,7 @@
 from pytocl.driver import Driver
 from pytocl.car import State, Command
+import numpy as np
+
 
 from config import *
 from utils.data import *
@@ -29,126 +31,87 @@ class rec_mode_driver(Driver):
     """
     def __init__(self, model_path):
         super().__init__()
+
         self.epoch = 0
+        self.max_gear = 6
+        self.expected_gear = 0
+        self.steering = 0
         self.jesus = MultiLayerPerceptron()
         self.jesus.restore(model_path)
+        self.recovery = False
+        self.max_unstuck_speed = 15
+        self.min_unstuck_dist = 5
+        self.max_unstuck_angle = 20
+
 
     def calc_gear(self, command, carstate):
-        acceleration = command.accelerator
-        if acceleration > 0:
-            if carstate.rpm > 8000:
-                command.gear = carstate.gear + 1
-        if carstate.rpm < 2500 and carstate.gear != 0:
-            command.gear = carstate.gear - 1
-        if not command.gear:
-            command.gear = carstate.gear or 1
+            if carstate.rpm > 7000 and carstate.gear < self.max_gear:
+                self.expected_gear = carstate.gear + 1
 
+            if carstate.rpm < 3000 and carstate.gear != 0:
+                self.expected_gear = carstate.gear - 1
+
+            if carstate.gear != self.expected_gear:
+                command.gear = self.expected_gear
+
+            if not command.gear:
+                command.gear = carstate.gear or 1
 
 
     def drive(self, carstate: State) -> Command:
 
         command = Command()
-        #print(carstate.angle)
-        #print(carstate.distance_from_center)
 
-        if -1 < carstate.distance_from_center < 1 and -80 < carstate.angle < 80:
+        if self.offTrack(carstate) == False:
+            self.jesus.restore('resources/models/mlp/')
             current_state = state_to_vector(carstate)
             command_vector = self.jesus.take_wheel(current_state)
             command = vector_to_command(command_vector)
             self.calc_gear(command, carstate)
-            if carstate.speed_x < 0 and carstate.rpm < 1000:
-                self.steering = 1
-                command.gear = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-
-            elif carstate.speed_x > 0 and carstate.rpm < 1000:
-                self.steering = -1
-                command.gear = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-
             self.epoch += 1
-            print(command)
-            return command
 
-        # Reposition car on track
-        if -1 < carstate.distance_from_center < 1 and -180 < carstate.angle < -80:
-                self.steeringering = -1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = 1
-                print('recmode1')
+        elif self.offTrack(carstate) == True:
+            print('Offtrack')
 
-        elif -1 < carstate.distance_from_center < 1 and 80 < carstate.angle < 180:
-                self.steeringering = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = -1
-                print('recmode2')
+            # Car points towards track center and is on LHS of track
+            if carstate.angle * carstate.distance_from_center > 0 and carstate.distance_from_center > 1:
+                self.steering = - carstate.angle
+                self.gear = 1
+                self.brake = 0
+                self.accelerate = 0.5
 
-        # Car on right side of track
-        elif carstate.distance_from_center < -1:
-            if 0 < carstate.angle <= 90:
-                self.steeringering = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = -1
-                print('recmode3')
+            # Car points towards track center and is on RHS of track
+            if carstate.angle * carstate.distance_from_center > 0 and carstate.distance_from_center < -1:
+                self.steering = - carstate.angle
+                self.gear = 1
+                self.brake = 0
+                self.accelerate = 0.5
 
-            elif 90 < carstate.angle < 180:
-                self.steering = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = -1
-                print('recmode4')
+            # Car points outwards and is on LHS of track
+            if carstate.angle * carstate.distance_from_center < 0 and carstate.distance_from_center > 1:
+                self.steering = - carstate.angle
+                self.gear = -1
+                self.brake = 0
+                self.accelerate = 0.5
 
+            # Car points outwards and is on RHS of track
+            if carstate.angle * carstate.distance_from_center < 0 and carstate.distance_from_center < -1:
+                self.steering = - carstate.angle
+                self.gear = -1
+                self.brake = 0
+                self.accelerate = 0.5
 
-            elif -90 < carstate.angle <= 0:
-                self.steering = 0.5
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = 1
-                print('recmode5')
-
-            elif -180 < carstate.angle <= -90:
-                self.steering = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = 1
-                print('recmode6')
-
-
-        # Car on left side of track
-        elif carstate.distance_from_center > 1:
-            if -180 < carstate.angle <= -90:
-                self.steering = -1
-                v_x = 15
-                self.accelerate(carstate, v_x, command)
-                command.gear = -1
-                print('recmode7')
-
-            elif -90 < carstate.angle < 0:
-                self.steering = 1
-                v_x = 30
-                self.accelerate(carstate, v_x, command)
-                command.gear = 2
-                print('recmode8')
-
-            elif 0 < carstate.angle < 90:
-                self.steering = 1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = 1
-                print('recmode9')
-
-            elif 90 < carstate.angle < 180:
-                self.steering = -1
-                v_x = 20
-                self.accelerate(carstate, v_x, command)
-                command.gear = 1
-                print('recmode10')
-
+        print(command)
         return command
 
+    def offTrack(self, carstate: State) -> Command:
 
+        if np.absolute(carstate.angle) > self.max_unstuck_angle and carstate.speed_x < self.max_unstuck_speed and np.abs(carstate.distance_from_center) > self.min_unstuck_dist:
+            if carstate.distance_from_center * np.absolute(carstate.angle) < 0:
+                self.recovery = True
+            else:
+                self.recovery = False
+        else:
+            self.recovery = False
+
+        return self.recovery
