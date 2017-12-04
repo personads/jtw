67dc1e2653d67eb222a396a67762fb2f7fb60a8d
cxxxw <33231216+cxxxw@users.noreply.github.com>
2017-12-04 12:27:40 +0100

    Add files via upload

---
+++ run/recmode_rnn.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import sys, os, argparse
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from pytocl.main import main
+from drivers.recmode import rec_mode_driver
+
+MODEL_PATH = 'resources/models/Link to BEST ONE/'
+
+if __name__ == '__main__':
+    main(rec_mode_driver(MODEL_PATH))
