de05b68d68bbf7e4b39e3fa5afeecba309c8c716
Kuroo <dkuric20@gmail.com>
2017-12-09 01:33:52 +0100

    Most random stuff ever

--- drivers/jesus.py
+++ drivers/jesus.py
@@ -35,6 +35,8 @@ class Jesus(Driver):
         # recovery
         self.recovery = False
         self.epochs_unmoved = 0
+        self.emer_brake = 0
+        self.last_gear_upshift = -100
 
 
     def calc_gear(self, command, carstate):
@@ -48,7 +50,11 @@ class Jesus(Driver):
             self.expected_gear = carstate.gear - 1
 
         if carstate.gear != self.expected_gear:
-            command.gear = self.expected_gear
+            if carstate.gear > self.expected_gear:
+                command.gear = self.expected_gear
+            elif carstate.gear < self.expected_gear and self.last_gear_upshift < self.epoch+20:
+                command.gear = self.expected_gear
+                self.last_gear_upshift = self.epoch
 
         if carstate.gear == 0 or carstate.gear == -1:
             command.gear = 1
@@ -69,54 +75,101 @@ class Jesus(Driver):
             return True
         return False
 
+    def emergency_brake(self, carstate: State, command: Command):
+
+        if carstate.distances_from_edge[9] < carstate.speed_x * 1.3 and carstate.speed_x > 60:
+            if command.accelerator > 0.1:
+                command.accelerator = 0
+            if self.epoch % 2 == 0 and self.emer_brake < 0.8 and command.brake < 0.2:
+                self.emer_brake += 0.2
+            command.brake = self.emer_brake
+            print("Distance from edge", carstate.distances_from_edge[9])
+            print("Speed x", carstate.speed_x)
+            print("Emergency Brake!!!", carstate.speed_x)
+        else:
+            self.emer_brake = 0
+
+
     def drive(self, carstate: State) -> Command:
         command = Command()
+        ANGLE_THRESHOLD = 10
+        BRAKING_CENTER_TRACK_THRESHOLD = 0.1
         # Check if car is stuck
         # Steering correction
         # 1)Forwards (car drives forward on track, but does not face toward center of track)
         self.recovery = self.is_stuck(carstate) if not self.recovery else self.recovery
         if self.recovery:
+            print("Recovery")
             self.epochs_unmoved = 0
-            if np.abs(carstate.angle) > 10:
+            if np.abs(carstate.angle) > ANGLE_THRESHOLD:
                 recovery_steering = 1
+                command.steering = recovery_steering if carstate.distance_from_center > 0 else -recovery_steering
+                command.gear = 1 if carstate.angle * carstate.distance_from_center > 0 else -1
+
                 if carstate.distance_from_center < 0:
                     if (carstate.speed_x < -1 and carstate.angle < 0) or (carstate.speed_x > 1 and carstate.angle > 0):
-                        command.brake = 1
-                        command.accelerator = 0
+                        if carstate.distance_from_center < -BRAKING_CENTER_TRACK_THRESHOLD:
+                            command.brake = 1
+                            command.accelerator = 0
+                        else:
+                            command.steering = -command.steering
                     else:
                         command.accelerator = 0.5
                         command.brake = 0
                 else:
                     if (carstate.speed_x < -1 and carstate.angle > 0) or (carstate.speed_x > 1 and carstate.angle < 0):
-                        command.brake = 1
-                        command.accelerator = 0
+                        if carstate.distance_from_center > BRAKING_CENTER_TRACK_THRESHOLD:
+                            command.brake = 1
+                            command.accelerator = 0
+                        else:
+                            command.steering = -command.steering
                     else:
                         command.accelerator = 0.5
                         command.brake = 0
-                command.steering = recovery_steering if carstate.distance_from_center > 0 else -recovery_steering
-                command.gear = 1 if carstate.angle * carstate.distance_from_center > 0 else -1
             else:
-
-                command.steering = -carstate.distance_from_center
+                command.steering = -0.5 if carstate.distance_from_center > 0 else 0.5
                 command.accelerator = .5
-                if not self.is_stuck(carstate):
-                    command.gear = 1
-                else:
-                    command.gear = -1
+                command.gear = -1
             # check if recovery complete
-            if -10 < carstate.angle < 10 and -1 < carstate.distance_from_center < 1:
+            if -ANGLE_THRESHOLD < carstate.angle < ANGLE_THRESHOLD:
                 self.recovery = False
         # NO RECOVERY (default behaviour if car is facing kind of straight)
         else:
+            print("Normal")
+
             current_state = state_to_vector(carstate)
             command_vector = self.holy_ghost.take_wheel(current_state)
             command = vector_to_command(command_vector)
             self.calc_gear(command, carstate)
-            apply_force_field(carstate, command)
+            #   apply_force_field(carstate, command)
+            #self.emergency_brake(carstate, command)
+
+            if np.abs(carstate.distance_from_center) > 1 and carstate.speed_x < 20:
+                command.brake = 0
+            if carstate.speed_x < -1:
+                command.brake = 1
+            if -1 < carstate.speed_x < 10 and np.abs(carstate.angle) < 90:
+                #print("Speeding assistant")
+                command.accelerator = 1
+                command.brake = 0
+            if carstate.distance_from_center > 1:
+                if carstate.angle < 20:
+                    command.steering = -0.1
+                elif carstate.angle > 30:
+                    command.steering = 0.1
+                #print("Steering assistant", self.epoch)
+            if carstate.distance_from_center < -1:
+                if carstate.angle < -20:
+                    command.steering = 0.1
+                elif carstate.angle < -30:
+                    command.steering = -0.1
+                #print("Steering assistant", self.epoch)
+            print(command)
+
         self.epoch += 1
-        print("unmoved:", self.epochs_unmoved)
-        print("angle:", carstate.angle)
-        print("distance center:", carstate.distance_from_center)
-        print("speed_x", carstate.speed_x)
-        print(command)
+        # print("unmoved:", self.epochs_unmoved)
+        # print("angle:", carstate.angle)
+        # print("distance center:", carstate.distance_from_center)
+        # print("speed_x", carstate.speed_x)
+        # print(command)
         return command
--- utils/swarm.py
+++ utils/swarm.py
@@ -29,14 +29,14 @@ def apply_force_field(state, command):
     dist_edges = collect_distances(state['distances_from_edge'])
     dist_edges_left = collect_distances(state['distances_from_edge'], [i for i in range(9)]) 
     dist_edges_right = collect_distances(state['distances_from_edge'], [i for i in range(10, 19)])
-    print("opponents distances:", dist_front, dist_right, dist_back, dist_left)
+    #print("opponents distances:", dist_front, dist_right, dist_back, dist_left)
     # if front opponent distances are < minimum
     if dist_front < OPPONENT_DISTANCE_MIN and dist_edges > dist_front:
         # if distance from edges > 100
-        print("opponents in front:", dist_front, dist_right, dist_back, dist_left)
+        #print("opponents in front:", dist_front, dist_right, dist_back, dist_left)
         if dist_edges > EDGE_DISTANCE_MIN and dist_edges > 0 and (dist_edges_right > EDGE_DISTANCE_MIN/2 or dist_edges_left > EDGE_DISTANCE_MIN/2):
             # steer in direction of largest opening
-            print("edges are far:", dist_edges, dist_edges_left, dist_edges_right)
+            #print("edges are far:", dist_edges, dist_edges_left, dist_edges_right)
             # steer additionally into current direction
             avoidance_steering = AVOIDANCE_STEERING
             if command.steering < 0:
@@ -53,30 +53,30 @@ def apply_force_field(state, command):
             # scale to distance
             avoidance_steering *= OPPONENT_DISTANCE_MIN/dist_front
             # add to steering set by holy ghost
-            print("adjusting steering by:", avoidance_steering)
+            #print("adjusting steering by:", avoidance_steering)
             command.steering += avoidance_steering
         # if edges and opponents are too close to avoid
         else:
             # and car is not off track
             if dist_edges > 0:
                 # adjust acceleration proportionally
-                print("edges are close:", dist_edges)
+                #print("edges are close:", dist_edges)
                 avoidance_decelaration = AVOIDANCE_DECELARATION
-                print("decelerating by:", avoidance_decelaration)
+                #print("decelerating by:", avoidance_decelaration)
                 command.brake += avoidance_decelaration
     # if distances to rear opponent are < minimum
     if dist_left < OPPONENT_DISTANCE_MIN and dist_edges_left > dist_left:
-        print("opponents on left:", dist_left)
+        #print("opponents on left:", dist_left)
         # if not already steering right
         if command.steering >= 0:
             if dist_edges > EDGE_DISTANCE_MIN and dist_edges > 0 and dist_right > OPPONENT_DISTANCE_MIN:
                 command.steering += -AVOIDANCE_STEERING
-                print("adjusted command steering:", command.steering)
+                #print("adjusted command steering:", command.steering)
     if dist_right < OPPONENT_DISTANCE_MIN and dist_edges_right > dist_right:
-        print("opponents on right:", dist_right)
+        #print("opponents on right:", dist_right)
         # if not already steering left
         if command.steering <= 0:
             if dist_edges > EDGE_DISTANCE_MIN and dist_edges > 0 and dist_left > OPPONENT_DISTANCE_MIN:
                 command.steering += AVOIDANCE_STEERING
-                print("adjusted command steering:", command.steering)
+                #print("adjusted command steering:", command.steering)
 
